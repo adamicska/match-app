5c34ca9fe4dbc17825e654a86fd4eb00e0899907
petu <adamicska>
2021-10-19 15:51:25 -0300

    sticky mobile footer fix

--- client/src/components/Navigation/Footer.js
+++ client/src/components/Navigation/Footer.js
@@ -17,8 +17,11 @@ function classNames(...classes) {
 const Footer = ({ auth: { isAuthenticated, loading }, logout }) => {
   const footerlinks = (
     <Fragment>
-      <Link to="/feed">
-        <button className="hover:bg-gray-100 px-3 py-2 flex justify-center">
+      <Link
+        to="/feed"
+        className="hover:bg-gray-100 px-3 py-2 flex justify-center"
+      >
+        <button>
           <NewspaperIcon className="h-6 w-6" />
         </button>
       </Link>
@@ -43,7 +46,7 @@ const Footer = ({ auth: { isAuthenticated, loading }, logout }) => {
 
   return (
     <Fragment>
-      <div className="absolute bottom-0 left-0 block w-full lg:hidden">
+      <div className="fixed bottom-0 left-0 block w-full lg:hidden bg-white ">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 border-t border-gray-200">
           <div className="grid grid-cols-3 divide-x divide-gray-200 h-12">
             {!isAuthenticated ? null : footerlinks}
